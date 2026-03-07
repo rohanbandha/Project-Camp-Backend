1756627287b8ecc4e3007f3508bfd36397cc6418
rohanbandha <rohansibuna@gmail.com>
2026-03-08 00:11:32 +0530

    Schema for projects, tasks and notes

---
+++ src/models/task.models.js
@@ -0,0 +1,39 @@
+import mongoose, { Schema } from "mongoose";
+import { AvailableTaskStatuses,TaskStatusEnum } from "../utils/constants";
+
+const taskSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: String,
+    project: {
+        type: Schema.Types.ObjectId,
+        ref: "Project",
+        required: true
+    },
+    assignedTo: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    assignedBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    status: {
+        type: String,
+        enum: AvailableTaskStatuses,
+        default: TaskStatusEnum.TODO
+    },
+    attachments: {
+        type: [{
+            url: String,
+            mimetype: String,
+            size: Number
+        }],
+        default: []
+    }
+}, {timestamps: true})
+
+export const Task = mongoose.model("Task", taskSchema)
